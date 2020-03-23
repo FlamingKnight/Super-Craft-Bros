65fdaa071cb9c26f85b93940d190175ac96e53f2
Flaming Knight <asherncc@gmail.com>
2020-03-23 14:29:06 -0700

    Forgot to use placing blocks as well

--- src/main/java/com/galacticdiamond/magma/supercraftbros/events/blocksevents/OpCanBreak.java
+++ src/main/java/com/galacticdiamond/magma/supercraftbros/events/blocksevents/OpCanBreak.java
@@ -5,6 +5,7 @@ import cn.nukkit.block.Block;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.block.BlockBreakEvent;
+import cn.nukkit.event.block.BlockPlaceEvent;
 import com.galacticdiamond.magma.supercraftbros.MagmaCore;
 import com.galacticdiamond.magma.supercraftbros.lists.CustomMessages;
 
@@ -18,7 +19,21 @@ public class OpCanBreak implements Listener {
     }
 
     @EventHandler
-    public void onPlace(BlockBreakEvent ev) {
+    public void onBreak(BlockBreakEvent ev) {
+        Block block = ev.getBlock();
+        Player player = ev.getPlayer();
+        if(!player.isOp() && player.getLevel().equals(plugin.getServer().getLevelByName("world"))) {
+            ev.setCancelled();
+        } else {
+            String playerUUIDStringified = String.valueOf(player.getUniqueId());
+            if(!player.getLevel().getName().equalsIgnoreCase(playerUUIDStringified + "-IslandWorld") && !player.isOp()) {
+                ev.setCancelled();
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlace(BlockPlaceEvent ev) {
         Block block = ev.getBlock();
         Player player = ev.getPlayer();
         if(!player.isOp() && player.getLevel().equals(plugin.getServer().getLevelByName("world"))) {
